e5737b9995a6ba7041c1fddc2c9f43bde7db056a
0kkun <shinji.okumoto.01@gmail.com>
2020-07-14 01:47:17 +0900

    コントローラー、ビューの作成

---
+++ server/resources/views/dbstudy/index.blade.php
@@ -0,0 +1,23 @@
+
+@extends('layouts.dbstudyapp')<!-- レイアウトの継承設定 -->
+
+<!-- 以下、親のテンプレートレイアウトに値を渡している。 -->
+
+@section('title','Index')
+
+@section('menubar')
+  <!-- @parent -->
+  インデックスページ
+@endsection
+
+
+
+@section('content')
+
+
+@endsection
+
+
+@section('footer')<!-- footerセクションにテキストを渡している -->
+  copylight 2020 okkun.
+@endsection
---
+++ server/resources/views/layouts/dbstudyapp.blade.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <title>@yield('title')</title>
+  <style>
+    th { background-color: #999; color: fff; padding: 5px 10px; }
+    td { border: solid 1px #aaa; color: #999; padding: 5px 10px; }
+  </style>
+</head>
+
+<body>
+  <h1>@yield('title')</h1>
+
+  @section('menubar')
+
+  <h2 class="menutitle"> メニュー </h2>
+  <ul>
+    <li>@show</li>
+  </ul>
+
+  <div class="content">
+    @yield('content')
+  </div>
+
+  <div class="footer">
+    @yield('footer')
+  </div>
+</body>
+
+</html>
